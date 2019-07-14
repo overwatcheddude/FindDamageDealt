155290a1af432f84bb42261df21e8b9931f1a49f
overwatcheddude <overwatcheddude@gmail.com>
2019-07-14 20:51:24 +0400

    User can search repeatedly

--- FindDamageDealt.py
+++ FindDamageDealt.py
@@ -3,22 +3,23 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen, Request
 import re #re stands for Regular Expression
 
-#Takes the user input.
-print("Enter URL:")
-URL = input()
-
 #Some websites do not allow crawlers or web scrapping. This header is a workaround and it prevents the 403 HTTP forbidden issue.
 HEADER = 'Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46'
 
-#A Request object is initialized using the URL and the header.
-request = Request(URL, data=None, headers={'User-Agent': HEADER})
+#This loop allows the user to input URLs repeatedly.
+while True:
+    #Takes the user input.
+    URL = input("Enter URL:")
 
-#BeautifulSoup object is intilized and the webpage is accessed here.
-soup = BeautifulSoup(urlopen(request), 'html.parser')
+    #A Request object is initialized using the URL and the header.
+    request = Request(URL, data=None, headers={'User-Agent': HEADER})
 
-#bool return either true or false. True if Damage Dealt is found, false if otherwise. The soup is the webpage, which is converted into string. Converting it to a string allows regex to read it.
-result = bool(re.search('Damage Dealt', str(soup)))
+    #BeautifulSoup object is intilized and the webpage is accessed here.
+    soup = BeautifulSoup(urlopen(request), 'html.parser')
 
-#If the result is true, then the title of the webpage that contains Damage Dealt strange part would be displayed. Else, nothing will be displayed to the user.
-if result:
-    print(soup.title.string)
+    #bool return either true or false. True if Damage Dealt is found, false if otherwise. The soup is the webpage, which is converted into string. Converting it to a string allows regex to read it.
+    result = bool(re.search('Damage Dealt', str(soup)))
+
+    #If the result is true, then the title of the webpage that contains Damage Dealt strange part would be displayed. Else, nothing will be displayed to the user.
+    if result:
+        print(soup.title.string)
